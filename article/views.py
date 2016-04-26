bb6c4cd15abb7e30cf632a867e12c07a035f0c7a
sirkor <dodkan@yandex.ru>
2016-04-26 13:35:58 +0300

    Test

--- article/views.py
+++ article/views.py
@@ -6,20 +6,7 @@ from django.shortcuts import render_to_response
 from article.models import Article,Comments
 # Create your views here.
 
-def basic_one(request):
-    view = "basic_one"
-    html = "<html><body>This is %s view</body></html>" %view
-    return HttpResponse(html)
 
-def template_two(request):
-    view = "template_two"
-    t = get_template('myview.html')
-    html = t.render(Context({'name':view}))
-    return HttpResponse(html)
-
-def template_three_simple(request):
-    view = "template_three_simple"
-    return render_to_response('myview.html',{'name': view})
 
 def articles(request):
     return  render_to_response('article/articles.html', {'articles':Article.objects.all()})
